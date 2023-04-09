ae784cbee0cea1d4bca0eccde64f292649a3996f
Daniyar <d.merekeyev@gmail.com>
2023-04-10 02:40:54 +0600

    first commit

---
+++ webApp/static/js/script.js
@@ -0,0 +1,9 @@
+function burgerMenuAction() {
+    let mobileMenu = document.getElementById("mobile-menu");
+
+    if (mobileMenu.style.display === "none") {
+        mobileMenu.style.display = "block";
+    } else {
+        mobileMenu.style.display = "none";
+    }
+}
